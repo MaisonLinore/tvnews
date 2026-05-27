369e20379c72602a892ccdd0d7be68bdafe2147b
MaisonLinore <mirfazli2016@gmail.com>
2026-05-27 20:36:25 +0700

    Create channels.js

---
+++ channels.js
@@ -0,0 +1,22 @@
+const channels = [
+  {
+    name: "RedBull TV",
+    url: "https://twitch.tv"
+  },
+  {
+    name: "DW News",
+    url: "https://youtube.com"
+  },
+  {
+    name: "NASA Live",
+    url: "https://ustream.tv"
+  },
+  {
+    name: "Sky News",
+    url: "https://dailymotion.com"
+  },
+  {
+    name: "GB News",
+    url: "https://dailymotion.com"
+  }
+];
