cc7354256a378ffa6820c2205cce18fff892ee71
MaisonLinore <mirfazli2016@gmail.com>
2026-05-27 21:49:00 +0700

    Update channels.js

--- channels.js
+++ channels.js
@@ -1,22 +1,27 @@
 const channels = [
   {
     name: "RedBull TV",
+    // Menggunakan link m3u8 pilihan Anda yang sudah terbukti lancar
     url: "https://rbmn-live.akamaized.net/hls/live/590964/BoRB-AT/master.m3u8"
   },
   {
     name: "DW News",
-    url: "https://www.youtube.com/embed/live_stream?channel=UCknLrEdhRCp1aEnv7nRrkWQ&autoplay=1&mute=1"
+    // Link m3u8 langsung alternatif untuk memotong masalah Iframe YouTube
+    url: "https://akamaized.net"
   },
   {
     name: "NASA Live",
-    url: "https://ustream.tv"
+    // Link streaming m3u8 resmi NASA TV Public Channel
+    url: "https://nasa-i.akamaihd.net/hls/live/253565/NASA-NTV1-Public/master.m3u8"
   },
   {
     name: "Sky News",
-    url: "https://dailymotion.com"
+    // Link m3u8 langsung CDN Sky News UK resmi
+    url: "https://linear417-gb-hls1-prd-ak.cdn.skycdp.com/100e/Content/HLS_001_1080_30/Live/channel(skynews)/index_1080-30.m3u8"
   },
   {
     name: "GB News",
-    url: "https://dailymotion.com"
+    // Link streaming m3u8 langsung untuk stasiun GB News Inggris
+    url: "https://live-gbnews.simplestreamcdn.com/s3/gbnews/index.m3u8"
   }
 ];
